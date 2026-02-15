0881a04c0a418c30c119fa5ccf7e9eeb9a9398ce
RecKatana <chimaevleha@gmail.com>
2026-02-15 10:25:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import telebot
+import os
 
+TOKEN = os.getenv("8215636432:AAFbkLxl2VSdFgLyho7uD12aITWMttfXZFQ")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Бот работает 🔥")
+
+print("Bot started...")
+bot.infinity_polling()
