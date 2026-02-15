86d5df9c110d7671575c0a445abc8e816f89247c
RecKatana <chimaevleha@gmail.com>
2026-02-15 10:58:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-import telebot
 import os
-from flask import Flask, request
+import telebot
+from flask import Flask
+from threading import Thread
 
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 app = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+@bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, "Бот работает 🔥")
 
-@app.route(f"/{TOKEN}", methods=["POST"])
-def webhook():
-    json_str = request.get_data().decode("UTF-8")
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return "OK", 200
-
-@app.route("/")
-def index():
-    return "Bot is running"
+def run_bot():
+    bot.infinity_polling()
 
 if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=f"https://sword-bot.onrender.com/{TOKEN}")
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    Thread(target=run_bot).start()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
